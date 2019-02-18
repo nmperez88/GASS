f464e5fff6c2c898f6ad28ceaf5518dd07a8a1de
nmperez88 <nmperez88@outlook.com>
2019-02-18 01:05:24 -0500

    Interface update 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  styleUrls: ['app.component.css']
+  styleUrls: ['app.component.scss']
 })
 export class AppComponent {
   public appPages = [
