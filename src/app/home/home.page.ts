160b325759327713fd50b204b83516c5af8264c1
Roy <roy@roy.com>
2019-02-18 19:28:41 -0300

    Mapa con marcador

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,61 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Geolocation} from '@ionic-native/Geolocation/ngx';
+import {LoadingController} from '@ionic/angular';
+
+declare var google;
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+    selector: 'app-home',
+    templateUrl: 'home.page.html',
+    styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
+    mapRef = null;
+
+    constructor(
+        private geolocation: Geolocation,
+        private loadingController: LoadingController
+    ) {
+    }
+
+    ngOnInit(): void {
+        this.loadMap();
+    }
+
+    async loadMap() {
+        const loading = await this.loadingController.create();
+        loading.present();
+
+        const myLatLng = await this.getLocation();
+        console.log(myLatLng);
+
+        const mapEle: HTMLElement = document.querySelector('#homeMap');
+        // create map
+        const map = new google.maps.Map(mapEle, {
+            center: myLatLng,
+            zoom: 12
+        });
+
+        google.maps.event.addListenerOnce(map, 'idle', () => {
+            loading.dismiss();
+            this.addMaker(myLatLng.lat, myLatLng.lng);
+        });
+    }
+
+    private addMaker(lat: number, lng: number) {
+        const marker = new google.maps.Marker({
+            position: { lat, lng },
+            map: this.mapRef,
+            title: 'Hello World!'
+        });
+    }
+
+    private async getLocation() {
+        const rta = await this.geolocation.getCurrentPosition();
+        return {
+            lat: rta.coords.latitude,
+            lng: rta.coords.longitude
+        };
+    }
 }
