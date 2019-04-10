7093be527666cb4402e2a57d25dfc2d82697b8fa
Roy <roy@roy.com>
2019-04-10 16:26:49 -0300

    Actualización

--- src/app/pages/registration/registration.page.ts
+++ src/app/pages/registration/registration.page.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import {ModalController} from '@ionic/angular';
 import {VerificationPage} from '../verification/verification.page';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { User } from '../../model/user';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-registration',
@@ -9,15 +12,69 @@ import {VerificationPage} from '../verification/verification.page';
 })
 export class RegistrationPage implements OnInit {
 
-  constructor(public modalController: ModalController ) {}
+  user: User = new User('', '', '', '', '', '');
+  password = '';
+  confirmPassword = '';
 
-  async verificationPage() {
-    const modal = await this.modalController.create({
-      component: VerificationPage,
-      cssClass: 'modalVerification',
-    });
-    await modal.present();
-  }
-  ngOnInit() {
-  }
+  constructor(
+    public modalController: ModalController,
+    public afAuth: AngularFireAuth,
+    public toastController: ToastController,
+   ) {}
+
+   ngOnInit() { }
+
+   async register() {
+     const { user } = this;
+
+     const toast = await this.toastController.create({
+       message: '',
+       duration: 2000,
+        showCloseButton: true,
+       // position: 'top',
+        closeButtonText: 'Cerrar'
+     });
+     if (this.password !== this.confirmPassword) {
+       toast.message = 'Las claves no coinciden';
+       toast.present();
+       return false;
+     }
+
+     const res = await this.afAuth.auth.createUserWithEmailAndPassword(user.email, this.password).catch(function (error) {
+       if (error.code === 'auth/email-already-in-use') {
+         toast.message = 'Existe una cuenta con el mismo correo';
+         toast.present();
+         return false;
+       }
+
+       if (error.code === 'auth/invalid-email') {
+         toast.message = 'Correo no válido';
+         toast.present();
+         return false;
+       }
+
+       if (error.code === 'auth/operation-not-allowed') {
+         toast.message = 'Cuenta deshabilitada';
+         toast.present();
+         return false;
+       }
+
+       if (error.code === 'auth/weak-password') {
+         toast.message = 'Clave muy débil';
+         toast.present();
+         return false;
+       }
+
+       return true;
+
+     });
+   }
+
+   async verificationPage() {
+     const modal = await this.modalController.create({
+       component: VerificationPage,
+       cssClass: 'modalVerification',
+     });
+     await modal.present();
+   }
 }
